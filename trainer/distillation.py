bd7fd0ac2c880658b9c3ddf2fecad8dc3e6f0deb
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-08 15:40:29 +0800

    .

--- trainer/distillation.py
+++ trainer/distillation.py
@@ -4,8 +4,8 @@ import logging
 from utils.dataset import ShardingLMDBDataset, cycle
 from utils.dataset import TextDataset
 from utils.distributed import EMA_FSDP, fsdp_wrap, fsdp_state_dict, launch_distributed_job
-from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
-from utils.lora import PeftModel
+from peft import PeftModel, LoraConfig, get_peft_model, prepare_model_for_kbit_training
+# from utils.lora import PeftModel
 
 from utils.misc import (
     set_seed,
