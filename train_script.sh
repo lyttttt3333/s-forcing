689dbabe523c67d4e44160812dbf364ed1fe09c6
Shiyi Lan <shiyil@nvidia.com>
2025-08-08 02:21:21 +0000

    .

--- train_script.sh
+++ train_script.sh
@@ -1,3 +1,4 @@
+export CUDA_VISIBLE_DEVICES=0,1
 torchrun --nnodes=1 --nproc_per_node=2 --master_port=29505 train.py \
   --config_path configs/self_forcing_dmd.yaml \
-  --logdir logs/self_forcing_6step_after_5000 \
+  --logdir logs/ \
