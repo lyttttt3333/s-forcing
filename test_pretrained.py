07017a738e9d76b641325e26dea022895a919889
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-09 10:30:05 +0800

    .

--- test_pretrained.py
+++ test_pretrained.py
@@ -1,16 +1,18 @@
 from utils.wan_wrapper import WanDiffusionWrapper, WanTextEncoder, WanVAEWrapper
 from wan.modules.causal_model import CausalWanModel
 from wan.modules.model import WanModel, RegisterTokens, GanAttentionBlock
+from utils.wan_wrapper import WanDiffusionWrapper, WanTextEncoder, WanVAEWrapper
 
+vae = WanVAEWrapper()
 # if is_causal:
 #     print(f"########### Loading from wan_models/{model_name}/")
 #     self.model = CausalWanModel.from_pretrained(
 #         f"wan_models/{model_name}/", local_attn_size=local_attn_size, sink_size=sink_size)
 # else:
-model_name = "Wan2.2-TI2V-5B"
-model = CausalWanModel.from_pretrained(f"wan_models/{model_name}/").to("cuda")
-model.requires_grad_(False)
-input("Done")
+# model_name = "Wan2.2-TI2V-5B"
+# model = CausalWanModel.from_pretrained(f"wan_models/{model_name}/").to("cuda")
+# model.requires_grad_(False)
+# input("Done")
 
 # real_model_name = "Wan2.2-TI2V-5B"
 # self.real_score = WanDiffusionWrapper(model_name=real_model_name, is_causal=False)
