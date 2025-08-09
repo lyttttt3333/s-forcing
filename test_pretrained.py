d12ad9a12bba889c3216e035944b5b37e9aaf834
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-09 09:51:04 +0800

    .

--- test_pretrained.py
+++ test_pretrained.py
@@ -8,7 +8,7 @@ from wan.modules.model import WanModel, RegisterTokens, GanAttentionBlock
 #         f"wan_models/{model_name}/", local_attn_size=local_attn_size, sink_size=sink_size)
 # else:
 model_name = "Wan2.2-TI2V-5B"
-model = WanModel.from_pretrained(f"wan_models/{model_name}/")
+model = CausalWanModel.from_pretrained(f"wan_models/{model_name}/")
 
 # real_model_name = "Wan2.2-TI2V-5B"
 # self.real_score = WanDiffusionWrapper(model_name=real_model_name, is_causal=False)
