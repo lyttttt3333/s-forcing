a61b70ec9de7ba8e44da50e59b7136e40a58b995
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-09 22:54:04 +0800

    .

--- utils/wan_wrapper_small.py
+++ utils/wan_wrapper_small.py
@@ -128,7 +128,7 @@ class WanDiffusionWrapper_small(torch.nn.Module):
             self.model = CausalWanModel.from_pretrained(
                 f"wan_models/{model_name}/", local_attn_size=local_attn_size, sink_size=sink_size, low_cpu_mem_usage=False, device_map=None)
         else:
-            self.model = WanModel.from_pretrained(f"wan_models/{model_name}/")
+            self.model = WanModel.from_pretrained(f"wan_models/{model_name}/",low_cpu_mem_usage=False, device_map=None)
         self.model.eval()
 
         # For non-causal diffusion, all frames share the same timestep
