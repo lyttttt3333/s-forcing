1073c07a3ce2fda872b5575c7f26c44866cc95ec
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-10 21:31:06 +0800

    .

--- wan/modules/vae.py
+++ wan/modules/vae.py
@@ -878,7 +878,7 @@ def _video_vae(pretrained_path=None, z_dim=48, dim=160, device="cpu", **kwargs):
 
     # load checkpoint
     logging.info(f"loading {pretrained_path}")
-    print("begin to load")
+    print(f"begin to load {pretrained_path}")
     state_dict = torch.load(pretrained_path, map_location=device)
     print(f"Done: loading {pretrained_path}")
     model.load_state_dict(
