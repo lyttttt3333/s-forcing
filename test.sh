f5fb0fe4fc1619159cf39b59f72c73fbf3c01c6e
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-11 00:20:16 +0800

    .

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-torchrun --nnodes=1 --nproc_per_node=8 --master_port=29505 test_pretrained.py 
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+torchrun --nnodes=1 --nproc_per_node=4 --master_port=29505 test_pretrained.py 
