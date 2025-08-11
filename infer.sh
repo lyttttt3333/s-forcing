306bdee8a83d22eece71081e2d6a2767a4fa981d
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-11 18:19:45 +0800

    .

--- infer.sh
+++ infer.sh
@@ -9,7 +9,7 @@ model_weights="/lustre/fsw/portfolios/av/users/shiyil/jfxiao/StreamVGGT/ckpt/${c
 
 output_dir="/lustre/fsw/portfolios/av/users/shiyil/jfxiao/StreamVGGT/eval_results/mv_recon/${model_name}_${ckpt_name}"
 echo "$output_dir"
-accelerate launch --num_processes 1 --main_process_port 29602 minimal_inference.py \
+accelerate launch --num_processes 1 --main_process_port 29502 minimal_inference.py \
     --weights "$model_weights" \
     --output_dir "$output_dir" \
     --model_name "$model_name" \
