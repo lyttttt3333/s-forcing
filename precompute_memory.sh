2b213ecbffc44cb12361c23bbcd400f4d0027019
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-11 17:59:40 +0800

    .

--- precompute_memory.sh
+++ precompute_memory.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-torchrun --nproc_per_node=6 precompute_memory.py
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5
+torchrun --nnodes=1 --nproc_per_node=6 --master_port=29505 precompute_memory.py
