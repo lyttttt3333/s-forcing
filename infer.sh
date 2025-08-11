5398b4bc0a6e9f847afce4f25695a0bed39a1999
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-11 18:22:11 +0800

    .

--- infer.sh
+++ infer.sh
@@ -6,7 +6,7 @@ model_name='StreamVGGT'
 ckpt_name='checkpoints'
 model_weights="/lustre/fsw/portfolios/av/users/shiyil/jfxiao/StreamVGGT/ckpt/${ckpt_name}.pth"
 
-
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 output_dir="/lustre/fsw/portfolios/av/users/shiyil/jfxiao/StreamVGGT/eval_results/mv_recon/${model_name}_${ckpt_name}"
 echo "$output_dir"
 accelerate launch --num_processes 1 --main_process_port 29572 minimal_inference.py \
