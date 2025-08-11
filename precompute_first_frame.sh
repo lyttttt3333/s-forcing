a55d95348b2c362fa3c2638c79660b1d1fde0e3f
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-11 21:34:07 +0800

    .

--- precompute_first_frame.sh
+++ precompute_first_frame.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-torchrun --nnodes=1 --nproc_per_node=8 --master_port=29505 precompute_memory.py
+torchrun --nnodes=1 --nproc_per_node=8 --master_port=29505 precompute_first_frame.py
