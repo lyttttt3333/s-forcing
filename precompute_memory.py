ec599069d4dc8ef9248314705d41438e9c1d554e
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-11 19:46:18 +0800

    .

--- precompute_memory.py
+++ precompute_memory.py
@@ -218,7 +218,8 @@ def main():
         print(f"[GPU {rank}] begin {base_name}")
         save_path = os.path.join(output_dir,f"{base_name}.pth")
         output_tokens = generate_tokens(model, frames_tensor)
-        torch.save(output_tokens, save_path)
+        print(output_tokens.shape, output_tokens.dtype)
+        # torch.save(output_tokens, save_path)
         print(f"[GPU {rank}] done {base_name}")
 
     dist.destroy_process_group()
