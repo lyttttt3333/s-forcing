32cb07f51d9a4891fc8059501e906e025695650e
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-11 19:07:51 +0800

    .

--- minimal_inference.py
+++ minimal_inference.py
@@ -79,7 +79,7 @@ def main(args):
         with torch.cuda.amp.autocast(dtype = torch.bfloat16):
             with torch.no_grad():
                 for i in range(frames.shape[0]):
-                    frame = frames[i]
+                    frame = frames[i].unsqueeze(0)
                     print(frame.shape)
                     aggregated_token, patch_start_idx, past_key_values = model.inference(frame, i, past_key_values=past_key_values)
                     print(aggregated_token.shape)
