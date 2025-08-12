5434babc521719fbaae466ea60dfbb7ec2a5de22
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-12 17:02:13 +0800

    .

--- utils/wan_wrapper.py
+++ utils/wan_wrapper.py
@@ -537,7 +537,7 @@ class WanDiffusionWrapper(torch.nn.Module):
         patch_size = (1, 2, 2)
         vae_stride = (4, 16, 16)
 
-        F = frame_num
+        F = 21
         seq_len = ((F - 1) // vae_stride[0] + 1) * (
             oh // vae_stride[1]) * (ow // vae_stride[2]) // (
                 patch_size[1] * patch_size[2])
