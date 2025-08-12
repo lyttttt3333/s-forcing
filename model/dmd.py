a78936efebbb86b14f7ef5489e06e72c90a94604
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-12 21:08:03 +0800

    .

--- model/dmd.py
+++ model/dmd.py
@@ -436,7 +436,7 @@ class DMD(SelfForcingModel):
             # sample videos
             latent = noise
             mask = torch.ones_like(noise)
-            mask1, mask2 = masks_like(noise, zero=False)
+            mask1, mask2 = masks_like([noise], zero=False)
             print(f"####### {mask2[0].shape} and {mask.shape}")
             # mask[:, 0] = 0
             mask = mask2[0]
