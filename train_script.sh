aef87cf63089f6f3b02cd13d9f101545999cc2ce
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-13 17:25:52 +0800

    .

--- train_script.sh
+++ train_script.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+git pull origin master
+
 export CUDA_VISIBLE_DEVICES=0,1,2,3 #,4,5,6,7
 torchrun --nnodes=1 --nproc_per_node=4 --master_port=29505 train.py \
   --config_path configs/self_forcing_dmd.yaml \
