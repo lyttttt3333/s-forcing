b2e9a615f348a8e3df35dc9e942c23143a928c2e
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-13 23:04:29 +0800

    。

--- train_script.sh
+++ train_script.sh
@@ -5,4 +5,4 @@ git pull origin master
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 torchrun --nnodes=1 --nproc_per_node=8 --master_port=29505 train.py \
   --config_path configs/self_forcing_gan.yaml \
-  --logdir logs/ \
+  --logdir logs/
