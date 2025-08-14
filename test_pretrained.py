a1efb76350c578d13a7ac5cb51a135252829f16f
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-15 01:38:09 +0800

    .

--- test_pretrained.py
+++ test_pretrained.py
@@ -36,7 +36,7 @@ def save_video(video_tensor, save_path, fps=30, quality=9, ffmpeg_params=None):
 vae = WanVAEWrapper()
 vae.requires_grad_(False)
 latent = torch.load("pred_image.pt", map_location="cpu").to("cuda")[0][:,:3]
-latent = torch.zeros([48, 1, 30, 60], device="cuda", dtype=torch.bfloat16) 
+latent = torch.zeros([48, 1, 30, 40], device="cuda", dtype=torch.bfloat16) 
 with torch.no_grad():
     # latent = vae.encode_to_latent([video])[0]
     print(latent.shape)
