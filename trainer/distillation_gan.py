fdbc71374253542d140ded4eb1dfaf00295426a7
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-15 15:41:37 +0800

    .

--- trainer/distillation_gan.py
+++ trainer/distillation_gan.py
@@ -515,7 +515,7 @@ class Trainer:
                 if self.generator_ema is not None:
                     self.generator_ema.update(self.model.generator)
             
-            if True:
+            if EVALUATION:
                 self.generate_video(self.step)
 
             # Train the critic
