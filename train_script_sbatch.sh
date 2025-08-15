522edf01d2edb8426a700269eedc3b17978de2ff
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-15 10:50:18 +0800

    。

--- train_script_sbatch.sh
+++ train_script_sbatch.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-#SBATCH --job-name=video_generation               # Job name
-#SBATCH --output=out_%j.txt             # Output file
-#SBATCH --error=err_%j.txt              # Error file
-#SBATCH --partition=a100                # Requested partition
-#SBATCH --gpus=4                       # Requested number of GPUs
-#SBATCH --time=20:00:00                 # Time limit (hh:mm:ss)
-#SBATCH --mail-type=BEGIN               # Send email when job starts
-#SBATCH --mail-use=liyitong.thu@gmail.com   # Destination Email
+#SBATCH --job-name=self_forcing_gan
+#SBATCH --partition interactive,polar,grizzly,polar3,polar4,backfill_singlenode,batch_singlenode,backfill_block1
+#SBATCH --account=av_alpamayo_research
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=128
+#SBATCH --gpus-per-node=8
+#SBATCH --mem=512G
+#SBATCH --time=12:10:00
+#SBATCH --output=logs/hello-%j.out
+#SBATCH --error=logs/hello-%j.err
 
 git pull origin master
+
+source /lustre/fsw/portfolios/av/users/shiyil/anaconda3/etc/profile.d/conda.sh
+conda activate self_forcing
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 torchrun --nnodes=1 --nproc_per_node=8 --master_port=29505 train.py \
