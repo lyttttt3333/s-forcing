43bfef0baa1d183f200d52ea41554d3f267ab3d1
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-18 17:40:39 +0800

    .

--- trainer/ode.py
+++ trainer/ode.py
@@ -257,9 +257,9 @@ class Trainer:
             ground_truth = ode_latent[0, -1]
             print("#############", input.shape, output.shape, ground_truth.shape)
 
-            input_video = self.model.vae.decode_to_pixel([input])
-            output_video = self.model.vae.decode_to_pixel([output])
-            ground_truth_video = self.model.vae.decode_to_pixel([ground_truth])
+            input_video = self.model.vae.decode_to_pixel([input])[0]
+            output_video = self.model.vae.decode_to_pixel([output])[0]
+            ground_truth_video = self.model.vae.decode_to_pixel([ground_truth])[0]
             input_video = 255.0 * (input_video.cpu().numpy() * 0.5 + 0.5)
             output_video = 255.0 * (output_video.cpu().numpy() * 0.5 + 0.5)
             ground_truth_video = 255.0 * (ground_truth_video.cpu().numpy() * 0.5 + 0.5)
