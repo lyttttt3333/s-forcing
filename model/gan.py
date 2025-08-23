c5a4a63e6041dc52e6ba54ee45e45e3f30de5d11
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-23 10:58:30 +0800

    .

--- model/gan.py
+++ model/gan.py
@@ -156,6 +156,10 @@ class GAN(SelfForcingModel):
         critic_timestep = torch.concatenate((critic_timestep, critic_timestep), dim=0)
         critic_timestep = critic_timestep * 0
         noisy_latent = torch.concatenate((noisy_fake_latent, noisy_real_latent), dim=0)
+
+        for p in self.fake_score.model.parameters():
+            p.requires_grad_(False) 
+
         _, _, noisy_logit = self.fake_score(
             noisy_image_or_video=noisy_latent,
             conditional_dict=conditional_dict,
